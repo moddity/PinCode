bd54e2498ff19a545a2cfe86582ad193b89aa821
Oriol Vilaró <ovilaro@bazingathree.home>
2011-06-20 12:00:19 +0200

    Initial Commit

---
+++ PinCode/PinCodeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PinCodeAppDelegate.h
+//  PinCode
+//
+//  Created by Oriol Vilaró on 20/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinCodeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
