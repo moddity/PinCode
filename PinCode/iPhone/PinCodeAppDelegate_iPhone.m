02865daf58f23f5f901c6109f5041d2d8d6eb822
Oriol Vilaró <ovilaro@bazingasystems.com>
2011-06-21 13:45:55 +0200

    ...

--- PinCode/iPhone/PinCodeAppDelegate_iPhone.m
+++ PinCode/iPhone/PinCodeAppDelegate_iPhone.m
@@ -3,7 +3,7 @@
 //  PinCode
 //
 //  Created by Oriol Vilaró on 20/06/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Bazinga Systems. All rights reserved.
 //
 
 #import "PinCodeAppDelegate_iPhone.h"
